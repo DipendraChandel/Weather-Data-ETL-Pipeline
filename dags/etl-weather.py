8550617151fdc6560093dfeb9968dbe4c5361efb
Dipendra <dipendrasingh2402@gmail.com>
2024-12-06 19:03:57 +0530

    new branch test commit

--- dags/etl-weather.py
+++ dags/etl-weather.py
@@ -27,7 +27,6 @@ with DAG (dag_id = 'weather_etl_pipeline',
     @task()
     def extract_weather_data():
         """Extracting weather data from Open-Meteo API using Airflow Connection"""
-        """Extracting weather data from Open-Meteo API using Airflow Connection -- test test"""
         
         # Use HTTP Hook to get connection details from Airflow Connection
         
